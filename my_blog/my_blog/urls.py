ea506504e40b462d61c19860127079d60d671e2a
pinkpink577 <499200305@qq.com>
2020-09-23 14:47:56 +0800

    feat.新建django项目

---
+++ my_blog/my_blog/urls.py
@@ -0,0 +1,25 @@
+"""my_blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # 参数article/分配了app的访问路径
+    # include 将路径分发给下一步处理
+    # namespace可以保证反查到唯一的url，即使不同的app使用了相同的url
+    path('article/', include('article.urls', namespace='article')),
+]
